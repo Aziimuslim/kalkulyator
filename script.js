eb5cbbe4393d02e5da9b225b371cc58dae172e7a
Aziimuslim <asqarovazizbek555@gmail.com>
2025-10-19 19:44:03 +0500

    Kalkulyator dasturi.

---
+++ script.js
@@ -0,0 +1,20 @@
+function append(char) {
+  document.getElementById("display").value += char;
+}
+
+function clearDisplay() {
+  document.getElementById("display").value = "";
+}
+
+function backspace() {
+  let current = document.getElementById("display").value;
+  document.getElementById("display").value = current.slice(0, -1);
+}
+
+function calculate() {
+  try {
+    document.getElementById("display").value = eval(document.getElementById("display").value);
+  } catch {
+    document.getElementById("display").value = "Xatolik";
+  }
+}
